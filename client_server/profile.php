d6589fe5d1accb48270ec22e6039bbc6f52c0e6f
sengarsameer <sengar.sameer@gmail.com>
2017-12-16 01:53:07 +0530

    update profile.php

---
+++ client_server/profile.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>LoginByOAuth</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+	</head>
+	<body>
+<div class="container">
+<h1>Your Profile</h1>
+     <div class="contact-form">
+	 <div class="signin">
+     <form>
+	     <h3>fetch all basic information here</h3> 
+     </form>
+	 </div>
+	 </div> 
+</div>
+</body>
+</html>
