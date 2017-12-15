5097213a51653f5d1d748c0b7b7fb9edba067766
SAMEER SENGAR <sengar.sameer@gmail.com>
2017-12-15 21:34:51 +0530

    Create index.php

---
+++ Oauth_server/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>LoginByOAuth</title>
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+</head>
+<body>
+<div class="container">
+<h1>An Application Would Like To Connect Your Account</h1>
+     <div class="contact-form">
+	 <div class="signin">
+     <form>
+	     <h2>Allow Sample App Access?</h2>
+         <input type="submit" value="Allow" />
+         <input type="submit" value="Deny" />						 
+     </form>
+	 </div>
+	 </div> 
+</div>
+<div class="footer">
+     <p>The app sample app by SAMEER SENGAR would like the ability to access your basic informations.</a></p>
+</div>
+</body>
+</html>
